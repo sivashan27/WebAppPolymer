f3045863efe534b5e8ee985c8d87a26e7efee910
Sivashanmugam P <Sivashanmugam.palanisamy@gmail.com>
2018-11-26 22:02:31 +0530

    First Commit

---
+++ WebAppPolymer/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebAppPolymer.Startup))]
+namespace WebAppPolymer
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
